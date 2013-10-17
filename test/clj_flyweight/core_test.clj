7d59660bb01054e8ad1570fe9dfdc8d7f961ea1a
Philip S Doctor <pdoctor@opinionlab.com>
2013-10-16 20:08:13 -0500

    stating test goal

--- test/clj_flyweight/core_test.clj
+++ test/clj_flyweight/core_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [clj-flyweight.core :refer :all]))
 
+"
+Goal: take a dsl and extract a sub-object
+
+{:first {:second [[:third \"third\"] [:fourth \"fourth\"]]}
+ :first-b #{:second-b :third-b}
+ :first-c [[:second-c] [:third-c]]
+ :first-d {:second-d :third-d}}
+
+"
+
 (deftest find-subset-test
   (testing "Finds a single subnode"
     (is (= (get-subset [:first] {:first "one"}) nil))))
