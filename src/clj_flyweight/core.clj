5af2989c3e5e6168494b908e8693e30e1eecfd43
Philip S Doctor <pdoctor@opinionlab.com>
2013-10-16 20:02:17 -0500

    initial code, requires large cleanup, clarification

--- src/clj_flyweight/core.clj
+++ src/clj_flyweight/core.clj
@@ -1,6 +1,23 @@
 (ns clj-flyweight.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn find-subset
+  [subset start-collection]
+    (loop [current-subset subset collection start-collection paused-search nil collection-agg []]
+      (let [first-current-subset (first current-subset) first-item-data (first collection)]
+        (cond
+          (and (= first-current-subset (first first-item-data)) (not-empty (rest current-subset)))
+              (recur (rest current-subset) (rest first-item-data) {:nested current-subset :col collection} collection-agg)
+          (= first-current-subset (first first-item-data))
+               (if (= first-item-data (rest collection))
+                 (conj collection-agg first-item-data)
+                 (recur current-subset (rest collection) paused-search (conj collection-agg first-item-data)))
+          (empty? first-item-data)
+              (if (empty? paused-search)
+                collection-agg
+                (recur (:nested paused-search) (rest (:col paused-search)) nil collection-agg))
+          :else (recur current-subset (rest collection) paused-search collection-agg)))))
+
+(defn get-subset
+  [subset collection]
+  (find-subset subset collection))
--- test/clj_flyweight/core_test.clj
+++ test/clj_flyweight/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [clj-flyweight.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest find-subset-test
+  (testing "Finds a single subnode"
+    (is (= (get-subset [:first] {:first "one"}) nil))))
